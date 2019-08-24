1785c3b078394e5136754b9945de94ff72cdde42
Talha Iqbal <talhaiqbal801@gamil.com>
2019-08-24 09:42:30 +0500

    Updated requests to filter away unnecessary data

--- server/server.js
+++ server/server.js
@@ -25,8 +25,9 @@ app.post('/shoppingLists', authenticate,  (req, res) => {
 
     (async function() {
         try {
-            var savedList = await shoppingList.save();  // Problem here
+            var savedList = await shoppingList.save();
             if(!savedList) return res.status(400).send();
+            savedList = savedList.toPrivateJSON();
             res.send({savedList});
         } catch(e) {
             res.status(400).send(e);
@@ -48,7 +49,7 @@ app.post('/shoppingLists/:listId', authenticate, (req, res) => {
 
     (async function() {
         try {
-            var updatedList = await ShoppingList.findOneAndUpdate(conditions, update, options).select('-creator_username');
+            var updatedList = await ShoppingList.findOneAndUpdate(conditions, update, options).select('-_creator -creator_username');
             if(!updatedList) return res.status(400).send();
             res.send({updatedList});
         } catch(e) {
@@ -66,15 +67,20 @@ app.patch('/shoppingLists/shareList/:listId', authenticate, (req, res) => {
 
     (async function() {
         try {
-            var userExists = await User.findOne({email: req.body.sharee_email});
-            if(!userExists) return res.status(400).send();
+            if(req.user.email === req.body.sharee_email) {
+                 return res.status(400).send();
+            }
 
-            var conditions = {_id: listId, _creator: req.user._id, sharee_email: null, shared: false};
-            var update = {$set: {sharee_email: req.body.sharee_email}};
+            var sharee = await User.findOne({username: req.body.username}).select('_id username');
+            if(!sharee) return res.status(400).send();
 
-            var list = await ShoppingList.findOneAndUpdate(conditions, update);
-            if(!list) return res.status(400).send();
-            res.send();
+            var conditions = {_id: listId, _creator: req.user._id, _sharee: null, shared: false};
+            var update = {$set: {_sharee: sharee._id, sharee_username: sharee.username}};
+            var options = {new: true};
+
+            var sharedList = await ShoppingList.findOneAndUpdate(conditions, update, options).select('-_creator -creator_username');
+            if(!sharedList) return res.status(400).send();
+            res.send({sharedList});
         } catch(e) {
             res.status(400).send(e);
         }
@@ -88,14 +94,14 @@ app.patch('/shoppingLists/unshareList/:listId', authenticate, (req, res) => {
         return res.status(404).send();
     }
 
-    var conditions = {_id: listId, _creator: req.user._id, sharee_email: {$ne: null}, shared: true};
-    var update = {$set: {sharee_email: null, shared: false}};
+    var conditions = {_id: listId, _creator: req.user._id, _sharee: {$ne: null}, shared: true};
+    var update = {$set: {_sharee: null, shared: false}};
 
     (async function() {
         try {
-            var list = await ShoppingList.findOneAndUpdate(conditions, update);
-            if(!list) return res.status(400).send();
-            res.send();
+            var unsharedList = await ShoppingList.findOneAndUpdate(conditions, update).select('-_creator -creator_username');
+            if(!unsharedList) return res.status(400).send();
+            res.send({unsharedList});
         } catch(e) {
             res.status(400).send(e);
         }
@@ -109,19 +115,21 @@ app.patch('/shoppingLists/received/unshareList/:listId', authenticate, (req, res
         return res.status(404).send();
     }
 
-    var conditions = {_id: listId, sharee_email: req.user.email, shared: true};
-    var update = {$set: {sharee_email: null, shared: false}};
+    var conditions = {_id: listId, _sharee: req.user._id, shared: true};
+    var update = {$set: {_sharee: null, shared: false}};
+    var options = {new: true};
 
     (async function() {
         try {
-            var list = await ShoppingList.findOneAndUpdate(conditions, update);
-            if(!list) return res.status(400).send();
-            res.send();
+            var unsharedList = await ShoppingList.findOneAndUpdate(conditions, update, options).select('-_sharee -sharee_username');
+            if(!unsharedList) return res.status(400).send();
+            res.send({unsharedList});
         } catch(e) {
             res.status(400).send(e);
         }
     })();
 });
+
 
 app.patch('/shoppingLists/acceptList/:listId', authenticate, (req, res) => {
     var listId = req.params.listId;
@@ -130,14 +138,15 @@ app.patch('/shoppingLists/acceptList/:listId', authenticate, (req, res) => {
         return res.status(404).send();
     }
 
-    var conditions = {_id: listId, sharee_email: req.user.email, shared: false};
+    var conditions = {_id: listId, _sharee: req.user._id, shared: false};
     var update = {$set: {shared: true}};
+    var options = {new: true};
 
     (async function() {
         try {
-            var list = await ShoppingList.findOneAndUpdate(conditions, update);
-            if(!list) return res.status(400).send();
-            res.send();
+            var acceptedList = await ShoppingList.findOneAndUpdate(conditions, update, options).select('-_sharee -sharee_username');
+            if(!acceptedList) return res.status(400).send();
+            res.send({acceptedList});
         } catch(e) {
             res.status(400).send(e);
         }
@@ -151,14 +160,15 @@ app.patch('/shoppingLists/rejectList/:listId', authenticate, (req, res) => {
         return res.status(404).send();
     }
 
-    var conditions = {_id: listId, sharee_email: req.user.email, shared: false};
-    var update = {$set: {sharee_email: null}};
+    var conditions = {_id: listId, _sharee: req.user._id, shared: false};
+    var update = {$set: {_sharee: null}};
+    var options = {new: true};
 
     (async function() {
         try {
-            var list = await ShoppingList.findOneAndUpdate(conditions, update);
-            if(!list) return res.status(400).send();
-            res.send();
+            var rejectedList = await ShoppingList.findOneAndUpdate(conditions, update, options).select('-_sharee -sharee_username');
+            if(!rejectedList) return res.status(400).send();
+            res.send({rejectedList});
         } catch(e) {
             res.status(400).send(e);
         }
@@ -179,7 +189,7 @@ app.patch('/shoppingLists/:listId', authenticate, (req, res) => {
 
     (async function() {
         try {
-            var updatedList = await ShoppingList.findOneAndUpdate(conditions, update, options).select('-creator_username');
+            var updatedList = await ShoppingList.findOneAndUpdate(conditions, update, options).select('-creator_username -_creator');
             if(!updatedList) return res.status(400).send();
             res.send({updatedList});
         } catch(e) {
@@ -203,7 +213,7 @@ app.patch('/shoppingLists/:listId/:itemId', authenticate, (req, res) => {
 
     (async function() {
         try {
-            var updatedList = await ShoppingList.findOneAndUpdate(conditions, update, options).select('-creator_username');
+            var updatedList = await ShoppingList.findOneAndUpdate(conditions, update, options).select('-creator_username -_creator');
             if(!updatedList) return res.status(400).send();
             res.send({updatedList});
         } catch(e) {
@@ -220,14 +230,14 @@ app.patch('/shoppingLists/received/:listId/:itemId', authenticate, (req, res) =>
         return res.status(404).send();
     }
 
-    var conditions = {_id: listId, "list_items._id": itemId, sharee_email: req.user.email, shared: true};
+    var conditions = {_id: listId, "list_items._id": itemId, _sharee: req.user._id, shared: true};
     var body = _.pick(req.body, ['price_per_item', 'quantity_available']);
     var update = {$set: {"list_items.$.price_per_item": body.price_per_item, "list_items.$.quantity_available": body.quantity_available}};
     var options = {new: true};
 
     (async function() {
         try {
-            var updatedList = await ShoppingList.findOneAndUpdate(conditions, update, options);
+            var updatedList = await ShoppingList.findOneAndUpdate(conditions, update, options).select('-_sharee -sharee_username');
             if(!updatedList) return res.status(400).send();
             res.send({updatedList});
         } catch(e) {
@@ -241,7 +251,7 @@ app.get('/shoppingLists', authenticate, (req, res) => {
 
     (async function() {
         try {
-            var myLists = await ShoppingList.find(conditions).select('-creator_username');
+            var myLists = await ShoppingList.find(conditions).select('-creator_username -_creator');
             if(!myLists) return res.status(400).send();
             res.send({myLists});
         } catch(e) {
@@ -251,11 +261,11 @@ app.get('/shoppingLists', authenticate, (req, res) => {
 });
 
 app.get('/shoppingLists/requests', authenticate, (req, res) => {
-    var conditions = {sharee_email: req.user.email, shared: false};
+    var conditions = {_sharee: req.user._id, shared: false};
 
     (async function() {
         try{ 
-            var unacceptedLists = await ShoppingList.find(conditions);
+            var unacceptedLists = await ShoppingList.find(conditions).select('-_sharee -sharee_username');
             if(!unacceptedLists) res.status(400).send();
             res.send({unacceptedLists});
         } catch(e) {
@@ -265,11 +275,11 @@ app.get('/shoppingLists/requests', authenticate, (req, res) => {
 });
 
 app.get('/shoppingLists/received', authenticate, (req, res) => {
-    var conditions = {sharee_email: req.user.email, shared: true};
+    var conditions = {_sharee: req.user._id, shared: true};
 
     (async function() {
         try {
-            var receivedLists = await ShoppingList.find(conditions);
+            var receivedLists = await ShoppingList.find(conditions).select('-_sharee sharee_username');
             if(!receivedLists) return res.status(400).send();
             res.send({receivedLists});
         } catch(e) {
@@ -279,11 +289,11 @@ app.get('/shoppingLists/received', authenticate, (req, res) => {
 });
 
 app.get('/shoppingLists/shared', authenticate, (req, res) => {
-    var conditions = {_creator: req.user._id, sharee_email: {$ne: null}, shared: true};
+    var conditions = {_creator: req.user._id, _sharee: {$ne: null}, shared: true};
 
     (async function() {
         try {
-            var sharedLists = await ShoppingList.find(conditions).select('-creator_username');
+            var sharedLists = await ShoppingList.find(conditions).select('-creator_username -_creator');
             if(!sharedLists) return res.status(400).send();
             res.send({sharedLists});
         } catch(e) {
@@ -303,7 +313,7 @@ app.delete('/shoppingLists/:listId', authenticate, (req, res) => {
 
     (async function() {
         try {
-            var deletedList = await ShoppingList.findOneAndDelete(conditions).select('_id');
+            var deletedList = await ShoppingList.findOneAndDelete(conditions).select('-_creator -creator_username');
             if(!deletedList) return res.status(400).send();
             res.send({deletedList});
         } catch(e) {
@@ -326,9 +336,9 @@ app.delete('/shoppingLists/:listId/:itemId', authenticate, (req, res) => {
 
     (async function() {
         try {
-            var updatedListId = await ShoppingList.findOneAndUpdate(conditions, update, options).select('_id, list_items._id');
-            if(!updatedListId) return res.status(400).send();
-            res.send({updatedListId});
+            var updatedList = await ShoppingList.findOneAndUpdate(conditions, update, options).select('-_creator -creator_username');
+            if(!updatedList) return res.status(400).send();
+            res.send({updatedList});
         } catch(e) {
             res.status(400).send(e);
         }
